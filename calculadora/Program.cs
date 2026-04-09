043a14b5f0495e8bea199156112471ef3117b4a4
eusouarth <152619065+ArthurNoIfba@users.noreply.github.com>
2026-04-09 15:20:43 +0000

    Alteração nas Rotas

---
+++ calculadora/Program.cs
@@ -0,0 +1,72 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("http://localhost:8000");
+
+var app = builder.Build();
+
+int resultado = 0;
+
+app.MapGet("/", () => {
+    return  new { mensagem= "API em execução"};
+});
+
+app.MapGet("/calcula/{opcao}/{valor1}/{valor2}", (int opcao, int valor1, int valor2) => {
+   switch (opcao)
+    {
+        case 1:
+            resultado = valor1 + valor2;
+            return Results.Ok(new
+            {
+                operacao = "soma",
+                valor1,
+                valor2,
+                resultado
+            });
+
+        case 2:
+            resultado =  valor1 - valor2;
+            return Results.Ok(new
+            {
+                operacao = "subtracao",
+                valor1,
+                valor2,
+                resultado 
+            });
+
+        case 3:
+            resultado =  valor1 * valor2;
+            return Results.Ok(new
+            {
+                operacao = "multiplicacao",
+                valor1,
+                valor2,
+                resultado 
+            });
+
+        case 4:            
+            if (valor2 == 0)
+            {
+                return Results.BadRequest(new
+                {
+                    erro = "Não é possível realizar divisão por zero."
+                });
+            }
+
+            resultado = valor1 / valor2;
+            return Results.Ok(new
+            {
+                operacao = "divisao",
+                valor1,
+                valor2,
+                resultado 
+            });
+        
+        default:
+            return Results.BadRequest(new
+            {
+                erro = "Operação inválida. Utilize: soma, subtracao, multiplicacao ou divisao."
+            });
+    }
+});
+
+app.Run();
